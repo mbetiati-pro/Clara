f50bd4198a17b9670cd78d77a5fc63d19fbd43fa
mbetiati-pro <marcos@mbetiati.pro>
2026-08-18 09:38:35 -0300

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
       },
       items: [{
         name: NOME_ITEM,
-        description: "Duas reuni\u00f5es online com o Marcos Betiati e a entrega do plano em at\u00e9 10 dias \u00fateis.",
+        description: "Duas reuni\u00f5es online com o Marcos Betiati e a entrega do plano em at\u00e9 10 dias \u00fateis em uma reuni\u00e3o online.",
         quantity: 1,
         value: VALOR
       }],
